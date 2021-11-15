d9682c66cb2ad99969aeb9d1f0f7651cd2d6deea
Sergey Orudzhev <ose19988@gmail.com>
2021-11-15 21:17:28 +0300

    Init

---
+++ src/test/java/com/bell/BellApi/BellApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bell.BellApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
