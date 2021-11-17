9c9383162c1afee733af9283056a5f451260e948
Sergey Orudzhev <ose19988@gmail.com>
2021-11-17 23:41:54 +0300

    Fixed according to previous commits

--- src/main/resources/create-schema.sql
+++ src/main/resources/create-schema.sql
@@ -28,28 +28,45 @@ CREATE TABLE IF NOT EXISTS Office(
     FOREIGN KEY(organization_id) REFERENCES Organization(id)
 );
 
+CREATE TABLE IF NOT EXISTS Worker_position(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    version INTEGER NOT NULL,
+    position_name VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS Usr(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     version INTEGER NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     second_name VARCHAR(50),
     middle_name VARCHAR(50),
-    usr_position VARCHAR(50) NOT NULL,
     phone VARCHAR(30),
     is_identified BIT(1) NOT NULL,
+    document_id INTEGER,
+    citizenship_id INTEGER,
+    position_id INTEGER NOT NULL,
     office_id INTEGER NOT NULL,
-    FOREIGN KEY(office_id) REFERENCES Office(id)
+    FOREIGN KEY(citizenship_id) REFERENCES Country(id),
+    FOREIGN KEY(office_id) REFERENCES Office(id),
+    FOREIGN KEY(position_id) REFERENCES Worker_position(id)
+);
+
+CREATE TABLE IF NOT EXISTS DocName(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    version INTEGER NOT NULL,
+    doc_code INTEGER(10) NOT NULL,
+    doc_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Document(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     version INTEGER NOT NULL,
-    doc_code INTEGER(10) NOT NULL,
-    doc_name VARCHAR(50) NOT NULL,
     doc_number VARCHAR(20) NOT NULL,
     doc_date DATE NOT NULL,
-    citizenship_id INTEGER,
+    doc_name_id INTEGER NOT NULL,
     usr_id INTEGER NOT NULL,
-    FOREIGN KEY(citizenship_id) REFERENCES Country(id),
-    FOREIGN KEY(usr_id) REFERENCES Usr(id)
+    FOREIGN KEY(doc_name_id) REFERENCES DocName(id)
 );
+
+ALTER TABLE Usr ADD FOREIGN KEY(document_id) REFERENCES Document(id);
+ALTER TABLE Document ADD FOREIGN KEY(usr_id) REFERENCES Usr(id);
