2dde6d3dff1655160b9fcbcad8add2d8e35a8858
Sergey Orudzhev <ose19988@gmail.com>
2021-11-25 16:35:04 +0300

    Add filter

--- src/main/java/com/bell/BellApi/dao/OrganizationDao.java
+++ src/main/java/com/bell/BellApi/dao/OrganizationDao.java
@@ -1,11 +1,12 @@
 package com.bell.BellApi.dao;
 
+import com.bell.BellApi.dao.impl.filter.OrgFilter;
 import com.bell.BellApi.model.Organization;
 
 import java.util.List;
 
 public interface OrganizationDao {
-    List<Organization> getAll();
+    List<Organization> getAll(OrgFilter filter);
     Organization getById(Long id);
     void save(Organization organization);
     void update(Organization organization);
