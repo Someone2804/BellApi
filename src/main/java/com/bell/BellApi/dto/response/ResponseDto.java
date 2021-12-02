993b399378d73b0fa3162222761288b5dc0ea1f9
Sergey Orudzhev <ose19988@gmail.com>
2021-12-02 16:25:38 +0300

    init

---
+++ src/main/java/com/bell/BellApi/dto/response/ResponseDto.java
@@ -0,0 +1,13 @@
+package com.bell.BellApi.dto.response;
+
+public class ResponseDto {
+    private Object body;
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+}
